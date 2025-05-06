fc545c0b6bcce804428dbc921b8353c5d051e04b
swapna <swapnasiri.com>
2025-05-06 20:19:26 +0530

    conversatin added

---
+++ 04-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "please enter your username::" # text entered here will be added as  value to variable
+
+read  -s USERNAME  # here USERNAME is the variable name
+
+
+echo "please enter your password::"
+
+read -s PASSWORD
+
---
+++ 05-data-types.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+NUMBER=$1
+NUMBER=$2
+
+TIMESTAMP=$(date)
+echo "Script exceuted at: $timestamp"
+SUM=$(($NUMBER1+$NUMBER2))
+
+echo "SUM of $NUMBER1 and $NUMBER2 is: $SUM"
