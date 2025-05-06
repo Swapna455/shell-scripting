5a89d5752d6b5cf21e0816ec68fe599b0745063e
swapna <swapnasiri.com>
2025-05-06 20:28:28 +0530

    conversatin added

---
+++ 06-array.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+MOVIES=("pusha " "rrr "devara)
+#index start from ;0 size is 3
+
+echo "first name   :  ${MOVIES[0]}"
+echo "second name  :  ${MOVIES[1]}"
+echo "third name   :  ${MOVIES[2]}"
+
+echo "all movies are : ${movies[@]}"
