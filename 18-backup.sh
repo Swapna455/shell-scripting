9dcf4f1ab00dd100d21b5b55af0902455a6a0b13
swapna <swapnasiri.com>
2025-05-08 12:37:05 +0530

    conversatin added

--- 18-backup.sh
+++ 18-backup.sh
@@ -16,7 +16,7 @@ LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 
 USAGE(){
     #echo -e "$R USAGE:: $N sh 18-backup.sh <SOURCE_DIR> <DEST_DIR> <DAYS(Optional)>"
-    echo -e "$R USAGE:: $N backup <SOURCE_DIR> <DEST_DIR> <DAYS(Optional)>"
+    echo -e "$R USAGE:: $N sh 18-backup.sh <SOURCE_DIR> <DEST_DIR> <DAYS(Optional)>"
     exit 1
 }
 
